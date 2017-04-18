ad1e1eda118e919de4e2d0f028ee423c1f419f79
mcarlier <maelle-carlier@hotmail.fr>
2017-04-18 17:51:35 +0200

    add code raspberry

---
+++ Raspberry_LightManager/lightManager.py
@@ -0,0 +1,61 @@
+import wiringpi
+import time
+import math
+import OSC
+import threading
+
+io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
+io.pinMode(1,io.PWM_OUTPUT)
+io.pwmWrite(1,0)
+bright = 0
+brightTarget = 0;
+receive_address = '10.0.0.25', 9001
+
+io.pwmWrite(1,bright)
+
+def sign(a,b):
+    if(a < b):
+        return 1
+    else:
+        return -1
+
+def updateBright():
+    # global bright
+    # for i in range(bright,newBright,sign(bright,newBright)):
+    #     io.pwmWrite(1,int(newBright*600/100))
+    #     time.sleep(0.015)
+    # bright = newBright
+    global bright
+    if(bright!=brightTarget):
+        bright +=sign(bright,brightTarget)*1
+        io.pwmWrite(1,int(bright*700/100))
+        time.sleep(0.015)
+
+
+# OSC Server. there are three different types of server.
+s = OSC.ThreadingOSCServer(receive_address)
+# this registers a 'default' handler (for unmatched messages)
+s.addDefaultHandlers()
+
+# define a message-handler function for the server to call.
+def printing_handler(addr, tags, stuff, source):
+    global brightTarget
+    if addr=='/light':
+        print "New target light ", stuff[0]
+        brightTarget = int(stuff[0])
+
+s.addMsgHandler("/light", printing_handler)
+
+def main():
+    # Start OSCServer
+    print "Starting OSCServer"
+    st = threading.Thread(target=s.serve_forever)
+    st.start()
+    try:
+        while 1:
+            updateBright()
+    except KeyboardInterrupt:
+        s.running = False
+        print("Stop!")
+
+main()
