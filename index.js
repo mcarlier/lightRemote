3745167ee972ca5dfbddaead307b1f11025df78b
CARTIER <cartier@Macintosh.local>
2017-04-21 12:23:54 +0200

    attempt slider

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var osc = require('node-osc');
+var ip = require('ip');
 var express = require('express')
 var app = express()
+var slider = require('bootstrap-slider');
 
-var RaspIP = "10.0.0.44";
+var RaspIP = "packshot02.local";
 var RaspPort = "9001";
 
-var LocalIP = "0.0.0.0";
+var LocalIP = "Macintosh.local";
 var LocalPort = "9001";
+
+var myip = ip.address()
+
+
+var clientLocal = new osc.Client(LocalIP, parseInt(LocalPort));
+var clientRaspberry = new osc.Client(RaspIP, parseInt(RaspPort));
+
+
 
 app.use(express.static('www'));
 
@@ -23,15 +33,14 @@ var server = app.listen(8080, function () {
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
+
   getCurrentValue();
   var oscServer = new osc.Server(8080, '0.0.0.0');
   oscServer.on("message", function (msg, rinfo) {
         if(rinfo.address=='127.0.0.1'){
           if(msg[2][0]=="/light"){
             socket.broadcast.emit('message',{ content: 'brightness', val: msg[2][1] });
-            var client = new osc.Client(RaspIP, parseInt(RaspPort));
-            client.send('/light', msg[2][1], function () {
-            client.kill();
+            clientRaspberry.send('/light', msg[2][1], function () {
             console.log('Send /light %s to %s:%s', msg[2][1], RaspIP,RaspPort);
             });
           }
@@ -39,6 +48,7 @@ io.sockets.on('connection', function (socket) {
             console.log('get peopleInside %s, fullness %s',msg[2][1],msg[2][2]);
             socket.broadcast.emit('message',{ content: 'peopleInside', val: msg[2][1] });
             socket.broadcast.emit('message',{ content: 'fullness', val: msg[2][2] });
+            socket.broadcast.emit('message',{ content: 'ip', val: myip });
 
           }
           else if(msg[2][0]=="/peopleInside"){
@@ -56,9 +66,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('brightness',  function(message) {
         socket.broadcast.emit('message',{ content: 'brightness', val: message });
-        var client = new osc.Client(RaspIP, parseInt(RaspPort));
-        client.send('/light', message, function () {
-        client.kill();
+        clientRaspberry.send('/light', message, function () {
         console.log('Send /light %s to %s:%s', message, RaspIP,RaspPort);
         });
 
@@ -66,18 +74,14 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('peopleInside',  function(message) {
       socket.broadcast.emit('message',{ content: 'peopleInside', val: message });
-      var client = new osc.Client(LocalIP, parseInt(LocalPort));
-      client.send('/peopleInside', message, function () {
-      client.kill();
+      clientLocal.send('/peopleInside', message, function () {
       console.log('Send /peopleInside %s to %s:%s', message, LocalIP,LocalPort);
       });
     });
     socket.on('fullness',  function(message) {
       socket.broadcast.emit('message',{ content: 'peopleInside', val: message });
-      var client = new osc.Client(LocalIP, parseInt(LocalPort));
-      client.send('/fullness', message, function () {
-      client.kill();
-      console.log('Send /fullness %s to %s:%s', message, LocalIP,LocalPort);
+      clientLocal.send('/fullness', message, function () {
+        console.log('Send /fullness %s to %s:%s', message, LocalIP,LocalPort);
       });
     });
 
@@ -86,21 +90,12 @@ io.sockets.on('connection', function (socket) {
 
 server.listen(8080);
 
-var client = new osc.Client('127.0.0.1', 3333);
-client.send('/oscAddress', 200, function () {
-  client.kill();
-});
 
 function getCurrentValue() {
-  var client = new osc.Client(RaspIP, parseInt(RaspPort));
-  client.send('/getlight', '', function () {
-  client.kill();
+  clientRaspberry.send('/getlight', '', function () {
   console.log('Send /getlight to %s:%s', RaspIP,RaspPort);
   });
-
-  var client2 = new osc.Client(LocalIP, parseInt(LocalPort));
-  client2.send('/getdata', '', function () {
-  client2.kill();
-  console.log('Send /getdata to %s:%s', LocalIP,LocalPort);
+  clientLocal.send('/getdata', '', function () {
+    console.log('Send /getdata to %s:%s', LocalIP,LocalPort);
   });
 }
