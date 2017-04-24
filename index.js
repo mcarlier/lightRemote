e8d24932dfdcb7a520e2fb062dc1a86cb40bdee7
mcarlier <maelle-carlier@hotmail.fr>
2017-04-24 18:29:19 +0200

    Add animation example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // var osc = require('node-osc');
 var express = require('express')
 var app = express()
+var rn = require('random-number');
+
 var five = require("johnny-five"),
   board, button;
 
@@ -23,18 +25,30 @@ var server = app.listen(8080, function () {
 
 })
 var last = 0;
+var animation;
 var board = new five.Board();
 board.on("ready", function() {
   console.log("READY");
   led = new five.Led(9);
   var io = require('socket.io').listen(server);
   var oTween = {bright:0};
+  //led.brightness(last);
+   animation= new five.Animation(led);
 
-  led.brightness(last);
+  animation1(1);// vitesse moyenne, fades random sur tout spectre
+  //animation2(1,1); //Rapide, luminosité basse, effet,néon
+  //animation3() //Lent, cycle avec un palier.
+  //animation4(); //Rapide, luminosité haute, grésillement
+  //animation5(245,-1);//cycle sur tout le spectre oscillant à faible luminosité
+  //animation6();//vitesse rapide, bas du spectre
+
+/*  led.brightness(last);
   led.pulse({
     duration:3000,
     keyFrames: [220, 240]
   });
+*/
+
 
   //
   // led.pulse({
@@ -51,7 +65,7 @@ board.on("ready", function() {
       // .easing(TWEEN.Easing.Cubic.In)
       .to({bright:255}, 2000)
       .onUpdate(function() {
-        console.log(this.bright);
+        //console.log(this.bright);
         var tmp = Math.round(this.bright);
         if (tmp !== last) {
           led.brightness(tmp);
@@ -59,14 +73,14 @@ board.on("ready", function() {
         }
       })
       .start();
+
   //
-  //
-  //   function animate() {
-  //   // requestAnimationFrame(animate);
-  //     setImmediate(animate);
-  //     TWEEN.update();
-  //   }
-  //   animate();
+     function animate() {
+     // requestAnimationFrame(animate);
+       setImmediate(animate);
+       TWEEN.update();
+     }
+     //animate();
 
   io.sockets.on('connection', function (socket) {
       socket.on('brightness',  function(percent) {
@@ -79,3 +93,116 @@ board.on("ready", function() {
   });
   server.listen(8080);
 });
+
+function animation1(speed){
+ var key = rn({min:50,max:247, integer: true});
+ console.log("animation1",key, speed);
+ animation.enqueue({
+   easing: "inOutQuint",
+   currentSpeed : speed,
+   duration: 500,
+   cuePoints: [0,1],
+   keyFrames: [null,key],
+   oncomplete: function() {
+       var speedrd = rn({min:0.5,max:1});
+       animation1(speedrd)
+   },
+ });
+}
+
+  function animation2(speed,sign){
+   var key = rn({min:200,max:255, integer: true});
+   var key2 = rn({min:200,max:255, integer: true});
+   console.log("animation2",speed,key,key2);
+   animation.enqueue({
+     easing: "in-out-expo",
+     currentSpeed : speed,
+     duration: 1000,
+     cuePoints: [0, 0.4, 0.8, 1],
+     keyFrames: [null, key,240, key2],
+     oncomplete: function() {
+         speed+= sign*0.05;
+         if (speed>=1) {
+           sign=-1;
+         }
+         else if(speed<=0.5){
+           sign=1;
+         }
+         animation2(speed,sign)
+     },
+   });
+  }
+  function animation3(key,lastkey){
+    var key = rn({min:200,max:247, integer: true});
+    var key2 = rn({min:200,max:247, integer: true});
+    var key3 = rn({min:235,max:247, integer: true});
+
+    var speed = rn({min:0.5,max:1});
+    console.log("animation3",key,lastkey,speed);
+    animation.enqueue({
+      currentSpeed : speed,
+      duration: 7000,
+      cuePoints: [0,0.3,0.35,0.65,0.75,0.8,0.9,1],
+      keyFrames: [null,parseInt(key/4),key,0,key2,parseInt(key2/2),key3,key3],
+      oncomplete: function() {
+        console.log("done");
+        animation3()
+      },
+    });
+  }
+  function animation4(){
+  var speed = rn({min:0.5,max:1.5});
+   var key = rn({min:0,max:190, integer: true});
+   var key2 = rn({min:100,max:200, integer: true});
+   var key3= rn({min:200,max:240, integer: true});
+   console.log("animation4",key,key2,key3,speed);
+   animation.enqueue({
+     currentSpeed : speed,
+     easing: "in-out-expo",
+     duration: 2000,
+       cuePoints: [0,0.2,0.3,0.4, 0.6,0.7, 1],
+       keyFrames: [null,key,key2,key,key3,key,key],
+     oncomplete: function() {
+         animation4()
+     },
+   });
+  }
+  function animation5(key,sign){
+   var step;
+   if (key>200) {
+      step=rn({min:5,max:15, integer: true})
+   }
+   else{
+     step=rn({min:20,max:50, integer: true})
+   }
+   console.log("animation5",step,key, sign);
+   animation.enqueue({
+       currentSpeed: key/100,
+       duration: 1000,
+       cuePoints: [0,0.2,0.4,0.6, 0.8, 1],
+       keyFrames: [null,key,key+step,key,key-step,key],
+       oncomplete: function() {
+         key+= sign*step;
+         if (key>240) {
+           sign=-1;
+         }
+         else if(key<=60){
+           sign=1;
+         }
+         animation5(key,sign)
+       },
+   });
+  }
+  function animation6(){
+   var key = rn({min:150,max:246, integer: true});
+   console.log("animation6",key);
+   animation.enqueue({
+     easing: "inOutBounce",
+     duration: 300,
+     cuePoints: [0,1],
+     keyFrames: [null,key],
+     oncomplete: function() {
+         animation6()
+     },
+   });
+  }
